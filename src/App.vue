c7ba586c6e5e8b7517b1f34d6db5272430902340
Nayana Ramesh <107012@tfcorp.com>
2022-09-21 20:13:58 +0530

    vue 3rd project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <comp-a />
+</template>
+
+<script>
+import CompA from './components/CompA.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CompA,
+  },
+  data(){
+    return{
+      userName: "NAYANA S R",
+    };
+  },
+  provide(){
+    return{
+      userName: this.userName,
+    };
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
